2ad0e55e89f4a6ede5518eebdda2bb3a02898a75
김정태 <kimjeongtae@gimjeongtaeui-MacBookPro.local>
2020-03-03 22:43:50 +0900

    ios enable

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Administrator\Documents\flash-chat-flutter"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/kimjeongtae/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kimjeongtae/Documents/Flutter-Firebase-Chat"
+export "FLUTTER_TARGET=/Users/kimjeongtae/Documents/Flutter-Firebase-Chat/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kimjeongtae/development/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
